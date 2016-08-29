1c16928a84d0dc8f58de1fa40c0034bdb6c8e769
kelsioliver <kelsioliver@Users-MacBook.local>
2016-08-28 21:44:25 -0400

    Final project

---
+++ main.js
@@ -0,0 +1,37 @@
+$("document").ready(function(){	
+	var topSelected =false;
+
+	var bottomSelected =false;
+
+	var top = "";
+
+	var bottom = "";
+
+	$("#choose-1").click(function(){ 
+		top = $(".box1").find(".is-selected").attr("data-clothing");
+
+		topSelected = true;
+	});
+
+	$("#choose-2").click(function(){
+		bottom = $(".box2").find(".is-selected").attr("data-clothing");
+		
+		bottomSelected = true;
+	});
+
+	$("#match").click(checkMatch)
+
+	function checkMatch(){
+		if (topSelected === true && bottomSelected === true){
+			if (top === bottom){
+				alert("MATCH!")
+			} else {
+				alert("MIS-MATCH!")
+			}
+
+		} else {
+			alert("Please select a top and a bottom.")
+		}
+
+	} 
+})
---
+++ script.js
@@ -0,0 +1,11 @@
+var topSelected = false;
+var bottomSelected = false;
+var top = "";
+var bottom = "";
+
+$("#choose-1").click(findTop)
+
+function findTop(){
+	top = $(".box1")
+	console.log(top);
+}
